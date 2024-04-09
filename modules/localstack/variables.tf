103df6ac84cbf13b9ef0000d64b65828b0d12fd8
dsiguero <26867647+dsiguero@users.noreply.github.com>
2024-04-10 00:08:45 +0200

    fix: undo accidental removal of localstack module

---
+++ modules/localstack/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  type = string
+  default = "localstack-main"
+}
+
+variable "volume_dir" {
+  type = string
+  default = "~/.cache/localstack/volume"
+}
+
+variable "docker_version" {
+  type = string
+  default = "3.3"
+}
+
+variable "debug" {
+  type = string
+  default = "0"
+}
