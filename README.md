5ebcd5d834673107bb547fa5662aa47d07fd60a7
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-01-01 13:27:51 +0100

    feat: add script/python tool to do service discovery for generating SPA public/config.js (per environment)

--- README.md
+++ README.md
@@ -65,6 +65,64 @@ Then, use the profile configured above before running AWS CLI, as per AWS docs:
 export AWS_PROFILE=notifycal-localstack
 ```
 
+## Service Registration / Service Discovery
+
+### Registration
+
+We're using a simple approach where each stack will register itself if required. This is configurable with a boolean `register` flag in `stacks/<stack>/source.json`.
+
+The registration is against SSM, using a parameter name/key of the following shape:
+```
+/notifycal/<environment>/<stack>/url
+```
+
+### Discovery
+
+As this applies mostly (uniquely?) to SPAs, each SPA will need to define a `config.skel.js` file:
+
+Example `.skel.js` file:
+```js
+// It's not valid JS yet.
+window.globalConfig = {
+  GOOGLE_CLIENT_ID: ${googleClientId},
+  BACKEND_BASE_URL: ${backendBaseUrl},
+  STATIC_LANDING_URL: ${staticLandingUrl}
+};
+```
+
+We also have a `mappings.json` file in this repo that maps skel templated "values" to SSM parameters:
+
+```json
+{
+  "googleClientId": "/notifycal/${environment}/providers/google/oauth2/client_secret",   // Not stack-dependant
+  "backendBaseUrl": "/notifycal/${environment}/backend/url",
+  "staticLandingUrl": "/notifycal/${environment}/static-landing/url",
+}
+```
+
+Note how the mappings file also exposes `environment` as something to template/interpolate.
+
+Now we can run the `service-discovery.py` program/script to generate a final config file for a given environment. This scripts resolves the `.skel.js` mappings with `mappings.json`, `environment` with the environment name, and retrieves each key from SSM.
+
+```
+$ python service-registration/service-discovery.py \
+  --environment prod \
+  --skel_file frontend.skel.js \
+  --mappings_file mappings.json > config.js
+```
+
+If a key is either not found in the mapping.json file, or in SSM, we'll automatically set it to `null`. That should trigger a failure when the frontend tries to process the config.
+
+#### Sample generated config.js file
+```js
+window.globalConfig = {
+  GOOGLE_CLIENT_ID: 'XXXXX-XXXXXXX.apps.googleusercontent.com',
+  BACKEND_BASE_URL: "https://apidev.notifycal.com",
+  // Example, in case a key is not in SSM or cannot resolve the mapping.
+  STATIC_LANDING_URL: null    
+};
+```
+
 ## environments
 
 - local
---
+++ service-registration/service-discovery.py
@@ -0,0 +1,136 @@
+import sys
+import os
+import subprocess
+import json
+import re
+from string import Template
+import argparse
+
+
+def parse_args():
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(
+        description="Generate a configuration file with interpolated values from SSM."
+    )
+    parser.add_argument(
+        "--environment", required=True, help="Environment (e.g., 'prod', 'dev')"
+    )
+    parser.add_argument(
+        "--mappings_file", required=True, help="Path to the mappings JSON file"
+    )
+    parser.add_argument("--skel_file", required=True, help="Path to the skeleton file")
+    parser.add_argument(
+        "--aws_region", required=False, help="AWS region (e.g., 'eu-west-1')"
+    )
+
+    return parser.parse_args()
+
+
+def find_interpolation_keys(skel):
+    # A interpolation key is of this shape: ${keyToInterpolate}
+    # Use a regular expression to find all `${...}` placeholders
+    pattern = re.compile(r"\$\{(.*?)\}")
+    matches = pattern.findall(skel)
+
+    return matches
+
+
+def get_ssm_parameter(parameter_name, aws_region):
+    try:
+        command = [
+            "aws",
+            "ssm",
+            "get-parameter",
+            "--name",
+            parameter_name,
+            "--with-decryption",
+            "--query",
+            "Parameter.Value",
+            "--output",
+            "text",
+        ]
+
+        if aws_region:
+            command.extend(["--region", aws_region])
+
+        result = subprocess.run(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            check=True,
+        )
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        print(
+            f"Error fetching parameter {parameter_name}: {e.stderr.strip()}\n",
+            file=sys.stderr,
+        )
+        return None
+    except Exception as e:
+        print(f"Unexpected error: {e}\n", file=sys.stderr)
+        return None
+
+
+def interpolate_skeleton(skel, resolved):
+    """Substitute placeholders in the skeleton with resolved values."""
+    skel_template = Template(skel)
+    return skel_template.safe_substitute(resolved)
+
+
+def main(args):
+    # 0. Load mappings and skel files
+    with open(args.mappings_file, "r") as f:
+        mappings = json.load(f)
+
+    with open(args.skel_file, "r") as file:
+        skel = file.read()
+
+    # 1. find interpolation keys in the skel file
+    interpolation_keys = find_interpolation_keys(skel)
+    print("Found potential interpolations in file:", file=sys.stderr)
+    for placeholder in interpolation_keys:
+        print(f"- {placeholder}", file=sys.stderr)
+    print("", file=sys.stderr)  # New line
+
+    # 2. filter only the desired keys from mappings (source of truth)
+    resolved = {}
+    for key, value in mappings.items():
+        if key in interpolation_keys:
+            # 3. interpolate `environment` in the value (SSM parameter name)
+            ssm_path = Template(value).safe_substitute(environment=args.environment)
+            # 4. and retrieve the value from SSM
+            ssm_value = get_ssm_parameter(ssm_path, args.aws_region)
+            # 5. wrapping in single quotes to replace the whole value,
+            # and so we can set values to null too
+            if ssm_value is not None:
+                resolved[key] = f"'{ssm_value}'"
+
+    # 6. Check what won't be resolved and resolve to null
+    unresolved_keys = [x for x in interpolation_keys if x not in resolved.keys()]
+    if len(unresolved_keys) > 0:
+        print(
+            "Unresolved placeholders found (and automatically set to null):",
+            file=sys.stderr,
+        )
+        for placeholder in unresolved_keys:
+            resolved[placeholder] = "null"  # Move this bit up?
+            print(f"- {placeholder}", file=sys.stderr)
+        print("", file=sys.stderr)  # New line
+        print(
+            "Please ensure all mappings are correct, as well as the skeleton file.\n",
+            file=sys.stderr,
+        )
+    else:
+        print(
+            "All placeholders have been successfully interpolated.\n", file=sys.stderr
+        )
+
+    # 4. Interpolate skel file
+    final_config = interpolate_skeleton(skel, resolved)
+    print(final_config)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
