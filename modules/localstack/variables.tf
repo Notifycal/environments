22440d727ea59aa291bac2c6d0165b27c1ceb729
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-01-03 01:20:32 +0100

    chore: enable ssm in localstack

--- modules/localstack/variables.tf
+++ modules/localstack/variables.tf
@@ -21,7 +21,7 @@ variable "debug" {
 
 variable "enabled_services" {
   type    = list(string)
-  default = ["logs", "iam", "apigateway", "s3", "lambda", "dynamodb", "sqs", "sns"] 
+  default = ["logs", "iam", "apigateway", "s3", "lambda", "dynamodb", "ssm", "sqs", "sns"] 
 }
 
 variable "credentials" {
