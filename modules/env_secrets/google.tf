4d142e2748e696e2eadc9a49f4f845967f5c9b2d
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-09-11 22:38:57 +0200

    fix: use right parameter name

--- modules/env_secrets/google.tf
+++ modules/env_secrets/google.tf
@@ -21,7 +21,7 @@ resource "aws_ssm_parameter" "google_oauth_client_secret" {
 resource "aws_ssm_parameter" "google_tag_manager_id" {
   count = try(var.google_tag_manager_id, null) == null ? 0 : 1
 
-  name  = "/notifycal/${var.environment}/static-landing/providers/google/tag-manager/id"
+  name  = "/notifycal/${var.environment}/providers/google/tag-manager/id"
   type  = "String"
   value = var.google_tag_manager_id
 
